7be194f130f4fad4294bc5c966c858f62a325d42
Marcin Duda <marc.duda94@gmail.com>
2020-09-17 23:49:21 +0200

    install storybook

---
+++ src/components/HeaderUI/HeaderUI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+export const HeaderUI = () => {
+
+    return <div>
+        Jestem headerem.
+    </div>
+}
+
+// const Container = styled.div`
+//     height: 100px;
+//     display: flex;
+// `;
---
+++ src/components/HeaderUI/HeaderUI.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { HeaderUI } from './HeaderUI';
+
+export default {
+  title: 'UI/HeaderUI',
+  component: HeaderUI,
+};
+
+const Template = (args) => <HeaderUI {...args} />;
+
+export const Test3 = Template.bind({});
+Test3.args = {};
